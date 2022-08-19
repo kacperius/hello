3ecd8c58cd86662ecd59f31181a60ae2c848cd82
Poziomek77 <jakub.kuroczycki@gmail.com>
2022-08-19 11:03:04 +0200

    Initial Commit

---
+++ hello/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  hello
+//
+//  Created by Kuba Kuroczycki on 19/08/2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
